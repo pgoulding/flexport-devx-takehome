d465fb15d0f31dd99ff63646f2b6d901ec05bf26
Patrick Goulding <patrickgoulding@gmail.com>
2023-04-03 16:02:21 -0700

    testing app url pass from k8s

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,18 +3,20 @@ import pytest
 
 
 @pytest.mark.integration
-def test_health_endpoint():
+@pytest.mark.parametrize("app_url", [APP_URL])
+def test_health_endpoint(app_url):
     """
     Tests Health Endpoint while application is running
     """
-    response = requests.get("http://127.0.0.1:5000/health")
+    response = requests.get(app_url + "/:5000/health")
     assert response.status_code == 200
 
 
 @pytest.mark.integration
-def test_rps_endpoint():
+@pytest.mark.parametrize("app_url", [APP_URL])
+def test_rps_endpoint(app_url):
     """
     Tests the RPS endpoint wehile server is running
     """
-    response = requests.post("http://127.0.0.1:5000/rps", json={"move": "rock"})
+    response = requests.post(app_url + "/:5000/rps", json={"move": "rock"})
     assert response.status_code == 200
