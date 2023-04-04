fc8fb42ea33823e0c8993e4aba98da084a2ed0f6
Patrick Goulding <patrickgoulding@gmail.com>
2023-04-04 08:42:26 -0700

    change from app_url from param to env variable and add doc strings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,1 @@
-import pytest
 
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--app_url", action="store", default="http://localhost/", help="Application URL"
-    )
-
-
-@pytest.fixture(scope="session")
-def app_url(request):
-    return request.config.getoption("--app_url")
--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,20 +1,29 @@
 import requests
 import pytest
+import os
+
+app_url = os.environ.get("APP_URL")
 
 
 @pytest.mark.integration
-def test_health_endpoint(app_url):
+def test_health_endpoint():
     """
-    Tests Health Endpoint while application is running
+    Test the health endpoint of the application.
+
+    :param app_url: The URL of the running application.
+    :type app_url: str
     """
-    response = requests.get(app_url + "/:5000/health")
+    response = requests.get(app_url + "/health")
     assert response.status_code == 200
 
 
 @pytest.mark.integration
-def test_rps_endpoint(app_url):
+def test_rps_endpoint():
     """
-    Tests the RPS endpoint wehile server is running
+    Test the RPS endpoint of the application while the server is running.
+
+    :param app_url: The URL of the running application.
+    :type app_url: str
     """
-    response = requests.post(app_url + "/:5000/rps", json={"move": "rock"})
+    response = requests.post(app_url + "/rps", json={"move": "rock"})
     assert response.status_code == 200
